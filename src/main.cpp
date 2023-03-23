21a01bfd45832f66371a45ab9009582ac95eb734
timoknapp <mail@timoknapp.com>
2023-03-23 16:09:24 +0100

    feat: retrieve temperature value and print it to the console

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <Arduino.h>
 
+const int tempPin = 34; // the analog pin the TMP36's Vout (sense) pin is connected to
+int tempVal = 0;        // variable to store the value coming from the sensor
+float volts = 0.0;      // variable to store the voltage coming from the sensor
+float temp = 0.0;       // variable to store the temperature in celsius
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // make an LED blink every second
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  // read the temp sensor and store it in tempVal
+  tempVal = analogRead(tempPin);
+  // normalize by the maximum temperature raw reading range
+  volts = tempVal/1024.0;             
+  // calculate temperature celsius from voltage as per the equation found on the sensor spec sheet.
+  temp = (volts - 0.5) * 100;         
+  Serial.println("Current time: " + String(__DATE__) + " " + String(__TIME__) + ", Temperature is: " + String(temp) + " degrees C°");
+  // wait for 30 seconds or 30000 milliseconds before taking the next reading.
+  delay(30000);    
+  // delay(30000);                  
 }
