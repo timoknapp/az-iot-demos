aca4cdebccf70908bea215df893b61351ce97bd2
Nico Schiering <mail@nico-schiering.de>
2023-03-23 16:09:16 +0100

    Initial commit

---
+++ src/iot_configs.h
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef IOT_CONFIGS_H
+#define IOT_CONFIGS_H
+
+#define IOT_CONFIG_WIFI_SSID "your-wifi-name"
+#define IOT_CONFIG_WIFI_PASSWORD "your-wifi-pwd"
+#define IOT_CONFIG_IOTHUB_FQDN ""
+#define IOT_CONFIG_DEVICE_ID ""
+#define IOT_CONFIG_DEVICE_KEY ""
+#define TELEMETRY_FREQUENCY_MILLISECS 30000
+
+#endif /* IOT_CONFIGS_H */
