322993bef3092695651658544e56f4006a6a06d9
timoknapp <mail@timoknapp.com>
2023-03-23 18:14:39 +0100

    feat: setup WiFi connection and introduce secrets.h

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # Azure IoT Demos
 
+## ToDo's
+
 - Receive temperature data every 30 seconds and push it Azure IoT Hub
 - Make use of the Azure IoT Hub Device SDK (Azure SDK for embedded C)
 - Testing Azure IoT Hub Device Provisioning Service (DPS)
 - Testing Device Update for IoT Hub
 - Testing [IoT Hub device streams](https://learn.microsoft.com/en-us/azure/iot-hub/iot-hub-device-streams-overview)
+
+## Getting Started
+
+### Prerequisites
+
+- VS Code with PlatformIO extension
+- ESP32 or ESP8266 board
+- Temperature sensor (e.g. DHT22, TMP36, etc.)
+
+### Installing
+
+- Prepare your `src/secrets.h` file
+  - Execute the following command in the directory of this project: 
+    - `echo "#define SECRET_WIFI_SSID \"yourSSID\"\n#define SECRET_WIFI_PASSWORD \"yourWiFiPassword\"" > src/secrets.h`
+  - Replace `yourSSID` and `yourWiFiPassword` with your WiFi credentials.
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,44 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <ESP32Ping.h>
+#include "secrets.h"
+
+const char* host = "timoknapp.com";
 
 const int tempPin = 34; // the analog pin the TMP36's Vout (sense) pin is connected to
 int tempVal = 0;        // variable to store the value coming from the sensor
 float volts = 0.0;      // variable to store the voltage coming from the sensor
 float temp = 0.0;       // variable to store the temperature in celsius
 
+static void pingHost(const char* host) {
+  Serial.println("Pinging host " + String(host) + "...");
+
+  if(Ping.ping(host)) {
+    Serial.println("Success");
+  } else {
+    Serial.println("Error");
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  Serial.println();
+  Serial.print("Connecting to WiFi");
+
+  WiFi.begin(SECRET_WIFI_SSID, SECRET_WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    // Serial.print(String(WiFi.status()));
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected to WiFi with IP: ");
+  Serial.println(WiFi.localIP());
+
+  pingHost(host);
 }
 
 void loop() {
@@ -17,8 +48,9 @@ void loop() {
   // normalize by the maximum temperature raw reading range
   volts = tempVal/1024.0;             
   // calculate temperature celsius from voltage as per the equation found on the sensor spec sheet.
-  temp = (volts - 0.5) * 100;         
-  Serial.println("Current time: " + String(__DATE__) + " " + String(__TIME__) + ", Temperature is: " + String(temp) + " degrees C°");
+  temp = (volts - 0.5) * 100;
+
+  Serial.println("Current time: " + String(__DATE__) + " " + String(__TIME__) + ", Temperature: " + String(temp) + " degrees C°");
   // wait for 30 seconds or 30000 milliseconds before taking the next reading.
   delay(30000);    
   // delay(30000);                  
