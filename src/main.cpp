1e5c458b2c534d084be5beedf013215b0ef9f98e
timoknapp <mail@timoknapp.com>
2023-03-28 09:41:13 +0200

    feat: connect ESP to Azure IoT Hub

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,410 @@
-#include <Arduino.h>
+/*
+ * This is an Arduino-based Azure IoT Hub sample for ESPRESSIF ESP32 boards.
+ * It uses our Azure Embedded SDK for C to help interact with Azure IoT.
+ * For reference, please visit https://github.com/azure/azure-sdk-for-c.
+ *
+ * To connect and work with Azure IoT Hub you need an MQTT client, connecting, subscribing
+ * and publishing to specific topics to use the messaging features of the hub.
+ * Our azure-sdk-for-c is an MQTT client support library, helping composing and parsing the
+ * MQTT topic names and messages exchanged with the Azure IoT Hub.
+ *
+ * This sample performs the following tasks:
+ * - Synchronize the device clock with a NTP server;
+ * - Initialize our "az_iot_hub_client" (struct for data, part of our azure-sdk-for-c);
+ * - Initialize the MQTT client (here we use ESPRESSIF's esp_mqtt_client, which also handle the tcp
+ * connection and TLS);
+ * - Connect the MQTT client (using server-certificate validation, SAS-tokens for client
+ * authentication);
+ * - Periodically send telemetry data to the Azure IoT Hub.
+ *
+ * To properly connect to your Azure IoT Hub, please fill the information in the `iot_configs.h`
+ * file.
+ */
+
+// C99 libraries
+#include <cstdlib>
+#include <string.h>
+#include <time.h>
+
+// Libraries for MQTT client and WiFi connection
 #include <WiFi.h>
 #include <WiFiClient.h>
+#include <mqtt_client.h>
 #include <ESP32Ping.h>
+
+// Azure IoT SDK for C includes
+#include <az_core.h>
+#include <az_iot.h>
+#include <azure_ca.h>
+
+// Additional sample headers
+#include "AzIoTSasToken.h"
+#include "SerialLogger.h"
+#include "secrets.h"
+
+// Libraries for sensors
+#include <Arduino.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 
-#include "secrets.h"
+// When developing for your own Arduino-based platform,
+// please follow the format '(ard;<platform>)'.
+#define AZURE_SDK_CLIENT_USER_AGENT "c%2F" AZ_SDK_VERSION_STRING "(ard;esp32)"
 
+// Utility macros and defines
+#define sizeofarray(a) (sizeof(a) / sizeof(a[0]))
+#define NTP_SERVERS "pool.ntp.org", "time.nist.gov"
+#define MQTT_QOS1 1
+#define DO_NOT_RETAIN_MSG 0
+#define SAS_TOKEN_DURATION_IN_MINUTES 60
+#define UNIX_TIME_NOV_13_2017 1510592825
+#define TELEMETRY_FREQUENCY_MILLISECS 30000
+
+#define CET_TIME_ZONE 1
+#define CET_TIME_ZONE_DAYLIGHT_SAVINGS_DIFF 1
+
+#define GMT_OFFSET_SECS (CET_TIME_ZONE * 3600)
+#define GMT_OFFSET_SECS_DST ((CET_TIME_ZONE + CET_TIME_ZONE_DAYLIGHT_SAVINGS_DIFF) * 3600)
+
+// DHT22 sensor
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 #define DHTPIN 4        // DHT 22 digital pin
 
 // Initialize DHT sensor.
 DHT dht(DHTPIN, DHTTYPE);
 
-const char* host = "timoknapp.com";
+// Translate secrets.h defines into variables used by the sample
+static const char* ssid = SECRET_WIFI_SSID;
+static const char* password = SECRET_WIFI_PASSWORD;
+static const char* host = SECRET_IOTHUB_FQDN;
+static const char* mqtt_broker_uri = "mqtts://" SECRET_IOTHUB_FQDN;
+static const char* device_id = SECRET_DEVICE_ID;
+static const int mqtt_port = AZ_IOT_DEFAULT_MQTT_CONNECT_PORT;
 
-static void pingHost(const char* host) {
+static const char* pingHost = "timoknapp.com";
+
+// Memory allocated for the sample's variables and structures.
+static esp_mqtt_client_handle_t mqtt_client;
+static az_iot_hub_client client;
+
+static char mqtt_client_id[128];
+static char mqtt_username[128];
+static char mqtt_password[200];
+static uint8_t sas_signature_buffer[256];
+static unsigned long next_telemetry_send_time_ms = 0;
+static char telemetry_topic[128];
+static uint8_t telemetry_payload[100];
+static uint32_t telemetry_send_count = 0;
+
+#define INCOMING_DATA_BUFFER_SIZE 128
+static char incoming_data[INCOMING_DATA_BUFFER_SIZE];
+
+// Auxiliary functions
+static AzIoTSasToken sasToken(
+    &client,
+    AZ_SPAN_FROM_STR(SECRET_DEVICE_KEY),
+    AZ_SPAN_FROM_BUFFER(sas_signature_buffer),
+    AZ_SPAN_FROM_BUFFER(mqtt_password));
+
+static void connectToWiFi()
+{
+  Logger.Info("Connecting to WIFI SSID " + String(ssid));
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Logger.Info("WiFi connected, IP address: " + WiFi.localIP().toString());
+}
+
+static void initializeTime()
+{
+  Logger.Info("Setting time using SNTP");
+
+  configTime(GMT_OFFSET_SECS, GMT_OFFSET_SECS_DST, NTP_SERVERS);
+  time_t now = time(NULL);
+  while (now < UNIX_TIME_NOV_13_2017)
+  {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  Logger.Info("Time initialized!");
+}
+
+void receivedCallback(char* topic, byte* payload, unsigned int length)
+{
+  Logger.Info("Received [");
+  Logger.Info(topic);
+  Logger.Info("]: ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println("");
+}
+
+static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
+{
+  switch (event->event_id)
+  {
+    int i, r;
+
+    case MQTT_EVENT_ERROR:
+      Logger.Info("MQTT event MQTT_EVENT_ERROR");
+      break;
+    case MQTT_EVENT_CONNECTED:
+      Logger.Info("MQTT event MQTT_EVENT_CONNECTED");
+
+      r = esp_mqtt_client_subscribe(mqtt_client, AZ_IOT_HUB_CLIENT_C2D_SUBSCRIBE_TOPIC, 1);
+      if (r == -1)
+      {
+        Logger.Error("Could not subscribe for cloud-to-device messages.");
+      }
+      else
+      {
+        Logger.Info("Subscribed for cloud-to-device messages; message id:" + String(r));
+      }
+
+      break;
+    case MQTT_EVENT_DISCONNECTED:
+      Logger.Info("MQTT event MQTT_EVENT_DISCONNECTED");
+      break;
+    case MQTT_EVENT_SUBSCRIBED:
+      Logger.Info("MQTT event MQTT_EVENT_SUBSCRIBED");
+      break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+      Logger.Info("MQTT event MQTT_EVENT_UNSUBSCRIBED");
+      break;
+    case MQTT_EVENT_PUBLISHED:
+      Logger.Info("MQTT event MQTT_EVENT_PUBLISHED");
+      break;
+    case MQTT_EVENT_DATA:
+      Logger.Info("MQTT event MQTT_EVENT_DATA");
+
+      for (i = 0; i < (INCOMING_DATA_BUFFER_SIZE - 1) && i < event->topic_len; i++)
+      {
+        incoming_data[i] = event->topic[i];
+      }
+      incoming_data[i] = '\0';
+      Logger.Info("Topic: " + String(incoming_data));
+
+      for (i = 0; i < (INCOMING_DATA_BUFFER_SIZE - 1) && i < event->data_len; i++)
+      {
+        incoming_data[i] = event->data[i];
+      }
+      incoming_data[i] = '\0';
+      Logger.Info("Data: " + String(incoming_data));
+
+      break;
+    case MQTT_EVENT_BEFORE_CONNECT:
+      Logger.Info("MQTT event MQTT_EVENT_BEFORE_CONNECT");
+      break;
+    default:
+      Logger.Error("MQTT event UNKNOWN");
+      break;
+  }
+
+  return ESP_OK;
+}
+
+static void initializeIoTHubClient()
+{
+  az_iot_hub_client_options options = az_iot_hub_client_options_default();
+  options.user_agent = AZ_SPAN_FROM_STR(AZURE_SDK_CLIENT_USER_AGENT);
+
+  if (az_result_failed(az_iot_hub_client_init(
+          &client,
+          az_span_create((uint8_t*)host, strlen(host)),
+          az_span_create((uint8_t*)device_id, strlen(device_id)),
+          &options)))
+  {
+    Logger.Error("Failed initializing Azure IoT Hub client");
+    return;
+  }
+
+  size_t client_id_length;
+  if (az_result_failed(az_iot_hub_client_get_client_id(
+          &client, mqtt_client_id, sizeof(mqtt_client_id) - 1, &client_id_length)))
+  {
+    Logger.Error("Failed getting client id");
+    return;
+  }
+
+  if (az_result_failed(az_iot_hub_client_get_user_name(
+          &client, mqtt_username, sizeofarray(mqtt_username), NULL)))
+  {
+    Logger.Error("Failed to get MQTT clientId, return code");
+    return;
+  }
+
+  Logger.Info("Client ID: " + String(mqtt_client_id));
+  Logger.Info("Username: " + String(mqtt_username));
+}
+
+static int initializeMqttClient()
+{
+  if (sasToken.Generate(SAS_TOKEN_DURATION_IN_MINUTES) != 0)
+  {
+    Logger.Error("Failed generating SAS token");
+    return 1;
+  }
+
+  esp_mqtt_client_config_t mqtt_config;
+  memset(&mqtt_config, 0, sizeof(mqtt_config));
+  mqtt_config.uri = mqtt_broker_uri;
+  mqtt_config.port = mqtt_port;
+  mqtt_config.client_id = mqtt_client_id;
+  mqtt_config.username = mqtt_username;
+
+  // Using SAS key
+  mqtt_config.password = (const char*)az_span_ptr(sasToken.Get());
+
+  mqtt_config.keepalive = 30;
+  mqtt_config.disable_clean_session = 0;
+  mqtt_config.disable_auto_reconnect = false;
+  mqtt_config.event_handle = mqtt_event_handler;
+  mqtt_config.user_context = NULL;
+  mqtt_config.cert_pem = (const char*)ca_pem;
+
+  mqtt_client = esp_mqtt_client_init(&mqtt_config);
+
+  if (mqtt_client == NULL)
+  {
+    Logger.Error("Failed creating mqtt client");
+    return 1;
+  }
+
+  esp_err_t start_result = esp_mqtt_client_start(mqtt_client);
+
+  if (start_result != ESP_OK)
+  {
+    Logger.Error("Could not start mqtt client; error code:" + start_result);
+    return 1;
+  }
+  else
+  {
+    Logger.Info("MQTT client started");
+    return 0;
+  }
+}
+
+/*
+ * @brief           Gets the number of seconds since UNIX epoch until now.
+ * @return uint32_t Number of seconds.
+ */
+static uint32_t getEpochTimeInSecs() { return (uint32_t)time(NULL); }
+
+static void establishConnection()
+{
+  connectToWiFi();
+  initializeTime();
+  initializeIoTHubClient();
+  (void)initializeMqttClient();
+}
+
+struct TemperatureData
+{
+  float temperatureC;
+  float temperatureF;
+  float humidity;
+};
+
+static TemperatureData readTemperature()
+{
+  float temperatureC = dht.readTemperature();
+  float temperatureF = dht.readTemperature(true);
+  float humidity = dht.readHumidity();
+
+  // // Compute heat index in Fahrenheit (the default)
+  // float heatIndexF = dht.computeHeatIndex(temperatureF, humidity);
+  // // Compute heat index in Celsius (isFahreheit = false)
+  // float heatIndexC = dht.computeHeatIndex(temperatureC, humidity, false);
+
+  if (isnan(temperatureC) || isnan(temperatureF) || isnan(humidity))
+  {
+    Logger.Error("Failed reading from DHT sensor!");
+    return { 0.0, 0.0, 0.0 };
+  }
+
+  Logger.Info("Temperature: " + String(temperatureC) + " °C (" + String(temperatureF) + " °F)");
+  Logger.Info("Humidity: " + String(humidity) + " %");
+
+  return { temperatureC, temperatureF, humidity };
+}
+
+static void getTelemetryPayload(az_span payload, az_span* out_payload)
+{
+  az_span original_payload = payload;
+
+  TemperatureData tempData = readTemperature();
+
+  Logger.Info("Temperature: " + String((int)tempData.temperatureC) + " °C (" + String((int)tempData.temperatureF) + " °F)" +  " Humidity: " + String((int)tempData.humidity) + " %");
+
+  // payload = az_span_copy(payload, AZ_SPAN_FROM_STR("{ \"temperatureCelcius\": 25.5, \"temperatureFahrenheit\": 99.5, \"humidity\": 55.5 }"));
+  // payload = az_span_copy_u8(payload, '\0');
+
+  payload = az_span_copy(payload, AZ_SPAN_FROM_STR("{ \"temperatureCelcius\": "));
+  (void)az_span_u32toa(payload, tempData.temperatureC, &payload);
+  payload = az_span_copy(payload, AZ_SPAN_FROM_STR(", "));
+  payload = az_span_copy(payload, AZ_SPAN_FROM_STR("\"temperatureFahrenheit\": "));
+  (void)az_span_u32toa(payload, tempData.temperatureF, &payload);
+  payload = az_span_copy(payload, AZ_SPAN_FROM_STR(", "));
+  payload = az_span_copy(payload, AZ_SPAN_FROM_STR("\"humidity\": "));
+  (void)az_span_u32toa(payload, tempData.humidity, &payload);
+  payload = az_span_copy(payload, AZ_SPAN_FROM_STR(", "));
+  payload = az_span_copy(payload, AZ_SPAN_FROM_STR("\"timestamp\": "));
+  (void)az_span_u32toa(payload, getEpochTimeInSecs(), &payload);
+  payload = az_span_copy(payload, AZ_SPAN_FROM_STR(" }"));
+  payload = az_span_copy_u8(payload, '\0');
+
+  *out_payload = az_span_slice(
+      original_payload, 0, az_span_size(original_payload) - az_span_size(payload) - 1);
+}
+
+static void sendTelemetry()
+{
+  az_span telemetry = AZ_SPAN_FROM_BUFFER(telemetry_payload);
+
+  Logger.Info("Sending telemetry ...");
+
+  // The topic could be obtained just once during setup,
+  // however if properties are used the topic need to be generated again to reflect the
+  // current values of the properties.
+  if (az_result_failed(az_iot_hub_client_telemetry_get_publish_topic(
+          &client, NULL, telemetry_topic, sizeof(telemetry_topic), NULL)))
+  {
+    Logger.Error("Failed az_iot_hub_client_telemetry_get_publish_topic");
+    return;
+  }
+
+  getTelemetryPayload(telemetry, &telemetry);
+
+  if (esp_mqtt_client_publish(
+          mqtt_client,
+          telemetry_topic,
+          (const char*)az_span_ptr(telemetry),
+          az_span_size(telemetry),
+          MQTT_QOS1,
+          DO_NOT_RETAIN_MSG)
+      == 0)
+  {
+    Logger.Error("Failed publishing");
+  }
+  else
+  {
+    Logger.Info("Message published successfully");
+  }
+}
+
+static void ping(const char* host) {
   Serial.println("Pinging host " + String(host) + "...");
 
   if(Ping.ping(host)) {
@@ -27,63 +415,34 @@ static void pingHost(const char* host) {
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 
   dht.begin();
 
-  Serial.println();
-  Serial.print("Connecting to WiFi");
+  establishConnection();
 
-  WiFi.begin(SECRET_WIFI_SSID, SECRET_WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    // Serial.print(String(WiFi.status()));
-    Serial.print(".");
-  }
-  Serial.println();
-  Serial.print("Connected to WiFi with IP: ");
-  Serial.println(WiFi.localIP());
-
-  pingHost(host);
+  ping(pingHost);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  // DHT22 sensor
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    connectToWiFi();
+  }
+  else if (sasToken.IsExpired())
+  {
+    Logger.Info("SAS token expired; reconnecting with a new one.");
+    (void)esp_mqtt_client_destroy(mqtt_client);
+    initializeMqttClient();
+  }
+  else if (millis() > next_telemetry_send_time_ms)
+  {
+    readTemperature();
+    sendTelemetry();
+    next_telemetry_send_time_ms = millis() + TELEMETRY_FREQUENCY_MILLISECS;
   }
 
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  Serial.print(String(__TIMESTAMP__)+" - ");
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%, Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.print("("+String(f));
-  Serial.print(F("°F), Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.print("("+String(hif));
-  Serial.println(F("°F)"));
-
   // wait for 30 seconds or 30000 milliseconds before taking the next reading.
-  delay(30000);
+  // delay(30000);
 }
